949d50aa9fbb13a0d25f347d17f01c170eec742c
Ravikant Vaishnav <luvkushvaishnav@gmail.com>
2023-04-17 17:32:48 +0530

    done by useContext

--- src/App.js
+++ src/App.js
@@ -1,61 +1,70 @@
 import Header from "./Components/Header/Header";
 import Items from "./Components/Items/Items";
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState, useContext, createContext } from "react";
 import Footer from "./Components/Footer/Footer";
+
+// creating context 
+const SkipAndLimit = createContext(null);
+
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
   const [limit, setLimit] = useState(10);
   const [skip, setSkip] = useState(0);
-  const [btnState , setBtnState] = useState(1);
+  const [btnState, setBtnState] = useState(1);
+
 
   let url = `https://bobsburgers-api.herokuapp.com/characters/?limit=${limit}&skip=${skip}`;
   const limitChangeHandler = (e) => {
     setLimit(e.target.value);
     console.log(e.target.value);
-  }
+  };
 
   const skipChangeHandler = (e) => {
     setSkip(e.target.value);
     console.log(e.target.value);
-  }
-
+  };
 
   useEffect(() => {
     console.log(url);
     fetchDataHandler();
-  }, [limit,skip])
+  }, [limit, skip]);
 
   const fetchDataHandler = (event) => {
-    fetch(url).then(response => {
-      return response.json();
-    }).then(data => {
-      setCharacters(data);
-    })
-  }
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCharacters(data);
+      });
+  };
 
-  const buttonClickHandler = (e)=>{
-    if(e.target.value === 'prev'){
+  const buttonClickHandler = (e) => {
+    if (e.target.value === "prev") {
       let newSkip = +skip - +limit;
-      if(newSkip < 0 ){
+      if (newSkip < 0) {
         newSkip = 0;
       }
       setSkip(newSkip);
-      setBtnState(prev => prev-1);
-    }
-    else{
+      setBtnState((prev) => prev - 1);
+    } else {
       let newSkip = +skip + +limit;
       setSkip(newSkip);
-      setBtnState(prev => prev+1);
+      setBtnState((prev) => prev + 1);
     }
-  }
+  };
   return (
-    <>
-      <Header setLimit = {limitChangeHandler} setSkip = {skipChangeHandler} skip={skip} limit={limit}/>
+    // provider value main jo jo pass krne hai pass kr do
+    <SkipAndLimit.Provider value={{skip, limit,skipChangeHandler,limitChangeHandler}} >
+      <Header/>
       <Items users={characters} />
-      <Footer btnState = {btnState} onClick = {buttonClickHandler} />
-    </>
+      <Footer btnState={btnState} onClick={buttonClickHandler} />
+    </SkipAndLimit.Provider>
   );
-}
+};
+
+// export kr do and fir vha pr import kr do
+export {SkipAndLimit};
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,21 @@
 import classes from './Header.module.css'
-import React, {useState} from 'react';
+import React, {useContext} from 'react';
+
+// importing 
+import { SkipAndLimit } from '../../App';
+
 const Header = (props) => {
-    
-    
+    // vo sab yha access kr rhe hai
+    const {skip, limit,skipChangeHandler:setSkip,limitChangeHandler:setLimit} = useContext(SkipAndLimit);
     return (
         <div className={classes.header}>
             <img src="https://bobs-burgers-api-ui.herokuapp.com/static/media/logo.fde474c9f99b4028a04f.png" alt="burger logo" />
 
             <div className={classes.frames}>
                 <label htmlFor='skips'>skips</label>
-                <input type="number" name="" id="skips" value={props.skip} onChange = {props.setSkip} />
+                <input type="number" name="" id="skips" value={skip} onChange = {setSkip} />
                 <label htmlFor="frame">Frams per Page</label>
-                <select id="frame"  onChange={props.setLimit} value={props.limit}>
+                <select id="frame"  onChange={setLimit} value={limit}>
                     <option value={10}>10</option>
                     <option value={15}>15</option>
                     <option value={20}>20</option>
